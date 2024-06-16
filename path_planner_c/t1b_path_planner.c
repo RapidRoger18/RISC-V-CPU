70961db82c5c0d559c2312d7beba29e9fb360c77
RapidRoger18 <Atharvakashalkar1804@gmail.com>
2024-06-16 20:28:23 +0530

    corrected Typo error while including a file

---
+++ path_planner_c/t1b_path_planner.c
@@ -0,0 +1,86 @@
+
+/*
+* AstroTinker Bot (AB): Task 1B Path Planner
+*
+* This program computes the valid path from the start point to the end point.
+* Make sure you don't change anything outside the "Add your code here" section.
+* Updated memory addresses for Task 2B
+*/
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __linux__ // for host pc
+
+    #include <stdio.h>
+
+    void _put_byte(char c) { putchar(c); }
+
+    void _put_str(char *str) {
+        while (*str) {
+            _put_byte(*str++);
+        }
+    }
+
+    void print_output(uint8_t num) {
+        if (num == 0) {
+            putchar('0'); // if the number is 0, directly print '0'
+            _put_byte('\n');
+            return;
+        }
+
+        if (num < 0) {
+            putchar('-'); // print the negative sign for negative numbers
+            num = -num;   // make the number positive for easier processing
+        }
+
+        // convert the integer to a string
+        char buffer[20]; // assuming a 32-bit integer, the maximum number of digits is 10 (plus sign and null terminator)
+        uint8_t index = 0;
+
+        while (num > 0) {
+            buffer[index++] = '0' + num % 10; // convert the last digit to its character representation
+            num /= 10;                        // move to the next digit
+        }
+
+        // print the characters in reverse order (from right to left)
+        while (index > 0) { putchar(buffer[--index]); }
+        _put_byte('\n');
+    }
+
+    void _put_value(uint8_t val) { print_output(val); }
+
+#else  // for the test device
+
+    void _put_value(uint8_t val) { }
+    void _put_str(char *str) { }
+
+#endif
+
+/*
+Functions Usage
+
+instead of using printf() function for debugging,
+use the below function calls to print a number, string or a newline
+
+for newline: _put_byte('\n');
+for string:  _put_str("your string here");
+for number:  _put_value(your_number_here);
+
+Examples:
+        _put_value(START_POINT);
+        _put_value(END_POINT);
+        _put_str("Hello World!");
+        _put_byte('\n');
+
+*/
+
+// main function
+int main() {
+        // Address value of variables are updated for RISC-V Implementation
+        #define LED_4         (* (volatile bool * ) 0x03000004)
+        LED_4 = true;
+    return 0;
+}
+
